8a88c0a38d6bcd78a15f1d9cf8f636eed4619fd1
nishant121222 <84307771+nishant121222@users.noreply.github.com>
2025-09-22 19:02:07 +0530

    Delete src directory

--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-// src/environments/environment.ts
-export const environment = {
-  production: false,
-  apiUrl: 'https://miramata.tech/reviewsystem/api/accounts/',
-  businessId: 1   // 👈 set your actual business_id here
-};
